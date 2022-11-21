0412cb659520d32725b2f06cec89016cd3f7ed66
rodrigo <jrodrigo0592@gmail.com>
2022-11-21 14:03:47 -0300

    refactor: refactoring main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './utils/transform.interceptor';
 
 async function bootstrap() {
+  const logger = new Logger('NestApplication');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new TransformInterceptor());
+  const port = process.env.PORT;
+  await app.listen(port);
+  logger.log(`Application listening on port ${port}`);
 }
 bootstrap();
