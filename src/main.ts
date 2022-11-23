63f22e61a68ba267d0e883f06414c438a25b615d
rodrigo <jrodrigo0592@gmail.com>
2022-11-23 12:12:09 -0300

    feat: setting swagger

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,25 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './utils/transform.interceptor';
 
 async function bootstrap() {
   const logger = new Logger('NestApplication');
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
+
+  const config = new DocumentBuilder()
+    .setTitle('Movie Catalog API')
+    .setDescription('Complete description and documentation')
+    .setVersion('1.0')
+    .addTag('movies')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const port = process.env.PORT;
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
